f222c1ac7ef0f81066a56c81dfa9581b8bc74d0e
fredsonPOSantos <son.magometal@gmail.com>
2024-12-03 11:21:21 -0300

    Update admin-app.js

--- js/admin-app.js
+++ js/admin-app.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://api-agendamento-idb2.onrender.com/api/appointments'; 
+const API_URL = 'https://fredsonposantos.github.io/API-Vercel/api/appointments'; 
 // Função para fazer login
 async function loginUser(event) {
     event.preventDefault();
